3f013d3c7e6b6fab841d1be87c5f05a6838ed3f8
Buck <ryan42buck@gmail.com>
2022-10-25 19:53:15 -0400

    heroku fix?

--- client/src/pages/NoMatch.js
+++ client/src/pages/NoMatch.js
@@ -5,7 +5,7 @@ import lost from "../assets/images/lost.png";
 const NoMatch = () => {
   return (
     <div className="lost-page">
-      <h1>That page doesn't exist!</h1>
+      <h1>That page doesn't exist! </h1>
       <img id="lost" src={lost} alt="lost"></img>
       <Link to={"/"}>Return Home</Link>
     </div>
